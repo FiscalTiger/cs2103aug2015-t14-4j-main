03f961f8666a1f8ad8c9ce39d70c4e06722fe63a
FiscalTiger <apouleson@gmail.com>
2015-10-08 00:02:59 +0800

    Switch to joda time objects in CalendarEvent objects (Run main for a chuckle)

--- src/easycheck/eventlist/CalendarEvent.java
+++ src/easycheck/eventlist/CalendarEvent.java
@@ -13,19 +13,21 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 public class CalendarEvent extends Event {
-	private static final String DATE_AND_TIME_OUTPUT_FORMAT = "E dd.mm.yyyy 'at' hh:mm a";
-	private static final String DATE_AND_TIME_INPUT_FORMAT = "dd.MMM.yyyy HH:mm";
+	private static final String DATE_AND_TIME_OUTPUT_FORMAT = "%s %d %s %d at %d:%02d";
+	private static final String DATE_AND_TIME_INPUT_FORMAT = "dd.MM.yyyy HH:mm";
 	private static final String MESSAGE_JSON_INPUT_ERROR = "Error parsing JSON Object from File at event: ";
 	private static final String MESSAGE_INVALID_DATE_STRING = "Error invalid date format";
 	private static final String MESSAGE_END_DATE_INPUT_ERROR = "Error while parsing new end date and time";
 	private static final String MESSAGE_START_DATE_INPUT_ERROR = "Error while parsing new start date and time";
 	private static final String MESSAGE_JSON_STRING_ERROR = "Error in toJsonString method, most likely coding error";
-	private static final String MESSAGE_TO_STRING_TEMPLATE = "%S from %s to %s\n";
+	private static final String MESSAGE_TO_STRING_TEMPLATE = "%s from %s to %s\n";
 	
 	private static final String JSON_TYPE = "type";
 	private static final String JSON_EVENT_INDEX = "index";
@@ -33,51 +35,39 @@ public class CalendarEvent extends Event {
 	private static final String JSON_START_DATE = "start";
 	private static final String JSON_END_DATE = "end";
 	
-	private Date startDateAndTime;
-	private Date endDateAndTime;
+	private static DateTimeFormatter fmt = DateTimeFormat.forPattern(DATE_AND_TIME_INPUT_FORMAT);
+	private DateTime startDateAndTime;
+	private DateTime endDateAndTime;
 	
 	public CalendarEvent(int eventIndex, String eventName, String startDateAndTimeString, String endDateAndTimeString) {
-		SimpleDateFormat ft =
-				new SimpleDateFormat(DATE_AND_TIME_INPUT_FORMAT);
 		this.setEventIndex(eventIndex);
 		this.setEventName(eventName);
-		try {
-			startDateAndTime = ft.parse(startDateAndTimeString);
-			endDateAndTime = ft.parse(endDateAndTimeString);
-		} catch(ParseException e) {
-			System.out.println(MESSAGE_INVALID_DATE_STRING);
-		}
+		startDateAndTime = fmt.parseDateTime(startDateAndTimeString);
+		endDateAndTime = fmt.parseDateTime(endDateAndTimeString);
 	}
 	
 	public CalendarEvent(JSONObject jsonObj) {
-		SimpleDateFormat ft = 
-			      new SimpleDateFormat (DATE_AND_TIME_INPUT_FORMAT);
 		Integer eventIndex = (Integer) jsonObj.get(JSON_EVENT_INDEX);
 		String eventName = (String) jsonObj.get(JSON_EVENT_NAME);
-		try {
-		
-		startDateAndTime = ft.parse((String)jsonObj.get(JSON_START_DATE));
-		endDateAndTime = ft.parse((String)jsonObj.get(JSON_END_DATE));
+		startDateAndTime = fmt.parseDateTime((String)jsonObj.get(JSON_START_DATE));
+		endDateAndTime = fmt.parseDateTime((String)jsonObj.get(JSON_END_DATE));
 		this.setEventIndex(eventIndex.intValue());
 		this.setEventName(eventName);
-		} catch(ParseException e) {
-			System.out.println(MESSAGE_JSON_INPUT_ERROR + eventName);
-		}
 	}
 	
 	/**
 	 * Returns the start date and time of this event
-	 * @return Date startDateAndTime
+	 * @return DateTime startDateAndTime
 	 */
-	public Date getStartDateAndTime() {
+	public DateTime getStartDateAndTime() {
 		return startDateAndTime;
 	}
 	
 	/**
 	 * Returns the end date and time of this event
-	 * @return Date endDateAndTime
+	 * @return DateTime endDateAndTime
 	 */
-	public Date getEndDateAndTime() {
+	public DateTime getEndDateAndTime() {
 		return endDateAndTime;
 	}
 	
@@ -87,13 +77,7 @@ public class CalendarEvent extends Event {
 	 * See Documentation for java.text.SimpleDateFormat
 	 */
 	public void setStartDateAndTime(String newDateString) {
-		SimpleDateFormat ft = 
-			      new SimpleDateFormat (DATE_AND_TIME_INPUT_FORMAT);
-		try {
-			startDateAndTime = ft.parse(newDateString);
-		} catch(ParseException e) {
-			System.out.println(MESSAGE_START_DATE_INPUT_ERROR);
-		}
+		startDateAndTime = fmt.parseDateTime(newDateString);
 	}
 	
 	/**
@@ -102,26 +86,36 @@ public class CalendarEvent extends Event {
 	 * See Documentation for java.text.SimpleDateFormat
 	 */
 	public void setEndDateAndTime(String newDateString) {
-		SimpleDateFormat ft = 
-			      new SimpleDateFormat (DATE_AND_TIME_INPUT_FORMAT);
-		Date temp;
-		try {
-			temp = ft.parse(newDateString);
-			assert(startDateAndTime.before(temp));
-			endDateAndTime = temp;
-		} catch(ParseException e) {
-			System.out.println(MESSAGE_END_DATE_INPUT_ERROR);
-		}
+		DateTime temp;
+		temp = fmt.parseDateTime(newDateString);
+		assert(startDateAndTime.isBefore(temp));
+		endDateAndTime = temp;
 	}
 	
 	/**
 	 * Returns the string form of this calendar event
 	 */
 	public String toString() {
-		SimpleDateFormat ft = 
-			      new SimpleDateFormat(DATE_AND_TIME_OUTPUT_FORMAT);
+		String startDateString = getFormattedStartDateString();
+		String endDateString = getFormattedEndDateString();
 		return String.format(
-				MESSAGE_TO_STRING_TEMPLATE, this.getEventName(), ft.format(startDateAndTime), ft.format(endDateAndTime));
+				MESSAGE_TO_STRING_TEMPLATE, this.getEventName(), startDateString, endDateString);
+	}
+
+	private String getFormattedStartDateString() {
+		DateTime.Property pDayOfTheWeek = startDateAndTime.dayOfWeek();
+		DateTime.Property pMonthOfYear = startDateAndTime.monthOfYear();
+		return String.format(DATE_AND_TIME_OUTPUT_FORMAT, pDayOfTheWeek.getAsShortText(),
+				startDateAndTime.getDayOfMonth(), pMonthOfYear.getAsShortText(), startDateAndTime.getYear(),
+				startDateAndTime.getHourOfDay(), startDateAndTime.getMinuteOfHour());
+	}
+	
+	private String getFormattedEndDateString() {
+		DateTime.Property pDayOfTheWeek = endDateAndTime.dayOfWeek();
+		DateTime.Property pMonthOfYear = endDateAndTime.monthOfYear();
+		return String.format(DATE_AND_TIME_OUTPUT_FORMAT, pDayOfTheWeek.getAsShortText(),
+				endDateAndTime.getDayOfMonth(), pMonthOfYear.getAsShortText(), endDateAndTime.getYear(),
+				endDateAndTime.getHourOfDay(), endDateAndTime.getMinuteOfHour());
 	}
 	
 	/**
@@ -129,14 +123,12 @@ public class CalendarEvent extends Event {
 	 * @return jsonString
 	 */
 	public String toJsonString() {
-		SimpleDateFormat ft = 
-			      new SimpleDateFormat(DATE_AND_TIME_INPUT_FORMAT);
 		Map obj=new LinkedHashMap();
 		obj.put(JSON_TYPE, "calendar");
 		obj.put(JSON_EVENT_INDEX, new Integer(this.getEventIndex()));
 		obj.put(JSON_EVENT_NAME, this.getEventName());
-		obj.put(JSON_START_DATE, ft.format(startDateAndTime));
-		obj.put(JSON_END_DATE, ft.format(endDateAndTime));
+		obj.put(JSON_START_DATE, fmt.print(startDateAndTime));
+		obj.put(JSON_END_DATE, fmt.print(endDateAndTime));
 		
 		StringWriter out = new StringWriter();
 	    try {
@@ -155,5 +147,10 @@ public class CalendarEvent extends Event {
 	// TODO override compareTo method
 	public int compareTo(Event e) {
 		return 0;
+	}
+	
+	public static void main(String[] args) {
+		CalendarEvent se = new CalendarEvent(1, "Banging Head Against Wall", "7.10.2015 12:00", "8.10.2015 00:00");
+		System.out.println(se);
 	}
 }
