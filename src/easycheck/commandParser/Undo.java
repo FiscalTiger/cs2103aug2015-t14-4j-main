8c72f2fe6fbca1fb50878d77b425212a58456b71
Thien Nguyen <nl.chuongthien@gmail.com>
2015-10-11 15:50:49 +0800

    created Undo Command Object

---
+++ src/easycheck/commandParser/Undo.java
@@ -0,0 +1,10 @@
+package easycheck.commandParser;
+
+public class Undo extends Command{
+
+	public Undo(String commandType, String[] arguments) {
+		super(commandType, arguments);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
