bafca17fe0c75360a534fa74a2c5d3c43e6d9eac
Teo Qi Xuan <teoqixuan@hotmail.com>
2015-10-20 23:32:01 +0800

    Commands now take exact amount of arguments

--- src/easycheck/commandParser/CommandParser.java
+++ src/easycheck/commandParser/CommandParser.java
@@ -6,6 +6,11 @@ package easycheck.commandParser;
  * 
  * @@author A0124206W
  */
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 public class CommandParser {
     private final String COMMAND_SPLITTER = " ";
     private final String ARGUMENT_SPLITTER = ",";
@@ -19,10 +24,8 @@ public class CommandParser {
 
     // command types
     private final String COMMAND_TYPE_ADD = "add";
-    private final String COMMAND_TYPE_ADD_EVENT = "addEvent";
     private final String COMMAND_TYPE_UPDATE = "update";
     private final String COMMAND_TYPE_DELETE = "delete";
-    
     // private final String COMMAND_TYPE_ADD_REPEAT = "addRepeat";
     // private final String COMMAND_TYPE_COMPLETE = "complete";
     // private final String COMMAND_TYPE_LABEL = "label";
@@ -40,30 +43,31 @@ public class CommandParser {
     private final String COMMAND_TYPE_EXIT = "exit";
 
     // expected number of parameters for all commands
-    private final int[] ARGUMENT_NUM_MIN_MAX_ADD = { 1, 2 };
-    private final int[] ARGUMENT_NUM_MIN_MAX_ADD_EVENT = { 2, 3 };
-    private final int[] ARGUMENT_NUM_MIN_MAX_UPDATE = { 2, 3 };
-    private final int[] ARGUMENT_NUM_MIN_MAX_DELETE = { 1, 1 };
-    
-    // private final int[] ARGUMENT_NUM_MIN_MAX_ADD_REPEAT = { 1, 2 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_COMPLETE = { 1, 1 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_LABEL = { 2, 2 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_REMIND = { 2, 2 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_SEARCH = { 1, 1 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_ARCHIVE = { 1, 1 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_STORE_LOCATION = { 1, 1 };
-    
-    private final int[] ARGUMENT_NUM_MIN_MAX_DISPLAY = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_ARCHIVE_DONE = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_DELETE_DONE = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_OVERDUE = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_DELETE_TODAY = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_NEXT = { 0, 0 };
-    // private final int[] ARGUMENT_NUM_MIN_MAX_UNDO = { 0, 0 };
-    private final int[] ARGUMENT_NUM_MIN_MAX_EXIT = { 0, 0 };
+    private final int NUM_ARGUMENTS_ADD = 1;
+    private final int NUM_ARGUMENTS_ADD_EVENT = 2;
+    private final int NUM_ARGUMENTS_UPDATE = 2;
+    private final int NUM_ARGUMENTS_DELETE = 1;
+
+    // private final int NUM_ARGUMENTS_ADD_REPEAT = 2;
+    // private final int NUM_ARGUMENTS_COMPLETE = 1;
+    // private final int NUM_ARGUMENTS_LABEL = 2;
+    // private final int NUM_ARGUMENTS_REMIND = 2;
+    // private final int NUM_ARGUMENTS_SEARCH = 1;
+    // private final int NUM_ARGUMENTS_ARCHIVE = 1;
+    // private final int NUM_ARGUMENTS_STORE_LOCATION = 1;
+
+    private final int NUM_ARGUMENTS_DISPLAY = 0;
+    // private final int NUM_ARGUMENTS_ARCHIVE_DONE = 0;
+    // private final int NUM_ARGUMENTS_DELETE_DONE = 0;
+    // private final int NUM_ARGUMENTS_OVERDUE = 0;
+    // private final int NUM_ARGUMENTS_DELETE_TODAY = 0;
+    // private final int NUM_ARGUMENTS_NEXT = 0;
+    // private final int NUM_ARGUMENTS_UNDO = 0;
+    private final int NUM_ARGUMENTS_EXIT = 0;
 
     // flexi command keywords
-    private final String[] FLEXI_KEYWORDS = {" by ", " to ", " until ", " for "};
+    private final String[] FLEXI_KEYWORDS = { " by ", " to ", " until ",
+            " for " };
 
     public Command parseCommand(String userCommand) {
         String[] commandArray = splitCommand(userCommand);
@@ -71,10 +75,15 @@ public class CommandParser {
             Command command = createCommand(commandArray[PARAM_POSITION_COMMAND_TYPE]);
             return command;
         } else {
-            Command command = createCommand(commandArray[PARAM_POSITION_COMMAND_TYPE],
+            Command command = createCommand(
+                    commandArray[PARAM_POSITION_COMMAND_TYPE],
                     commandArray[PARAM_POSITION_COMMAND_ARGUMENT]);
             return command;
         }
+    }
+
+    private String[] splitCommand(String userCommand) {
+        return userCommand.split(COMMAND_SPLITTER, COMMAND_ARRAY_LENGTH);
     }
 
     private Command createCommand(String commandType) {
@@ -84,53 +93,36 @@ public class CommandParser {
         } else {
             System.err.println(MESSAGE_UNRECOGNIZE);
         }
-        Command command = Command.createObject(commandType,arguments);
+        Command command = Command.createObject(commandType, arguments);
         return command;
-        
-    }
 
-    private String[] splitCommand(String userCommand) {
-        return userCommand.split(COMMAND_SPLITTER, COMMAND_ARRAY_LENGTH);
     }
 
     private Command createCommand(String commandType, String commandArguments) {
         String[] arguments = null;
         if (commandType.equalsIgnoreCase(COMMAND_TYPE_ADD)) {
-            arguments = getArguments(commandArguments, ARGUMENT_NUM_MIN_MAX_ADD);
-        } else if (commandType.equalsIgnoreCase(COMMAND_TYPE_ADD_EVENT)) {
-            arguments = getArguments(commandArguments,
-                    ARGUMENT_NUM_MIN_MAX_ADD_EVENT);
+            arguments = getArguments(commandArguments, NUM_ARGUMENTS_ADD);
         } else if (commandType.equalsIgnoreCase(COMMAND_TYPE_DELETE)) {
-            arguments = getArguments(commandArguments,
-                    ARGUMENT_NUM_MIN_MAX_DELETE);
+            arguments = getArguments(commandArguments, NUM_ARGUMENTS_DELETE);
         } else if (commandType.equalsIgnoreCase(COMMAND_TYPE_UPDATE)) {
-            arguments = getArguments(commandArguments,
-                    ARGUMENT_NUM_MIN_MAX_UPDATE);
+            arguments = getArguments(commandArguments, NUM_ARGUMENTS_UPDATE);
         } else {
             System.err.println(MESSAGE_UNRECOGNIZE);
         }
         assert (arguments != null);
-        Command command = Command.createObject(commandType,arguments);
+        Command command = Command.createObject(commandType, arguments);
         return command;
     }
 
-    private String[] getArguments(String commandArguments,
-            int[] expectedArguments) {
-        
-        String[] arguments = checkFlexi(commandArguments);
-        if (arguments.length < expectedArguments[0]) {
-            throw new Error(MESSAGE_INVALID_ARGUMENTS);
-        }
-        if (arguments.length >= expectedArguments[0]
-                && arguments.length <= expectedArguments[1]) {
-            // correct number of arguments
-            arguments = trimArguments(arguments, arguments.length);
-        } else if (arguments.length > expectedArguments[1]) {
-            //more than the maximum amount of arguments, truncate extra
-            String[] truncatedArguments = trimArguments(arguments, expectedArguments[1]);
-            return truncatedArguments;
-        }
+    private String[] getArguments(String commandArguments, int expectedArguments) {
 
+        String[] arguments = checkFlexi(commandArguments);
+        if (arguments.length < expectedArguments
+                || arguments.length > expectedArguments) {
+            throw new Error(MESSAGE_INVALID_ARGUMENTS);
+        } else {
+            
+        }
         return arguments;
     }
 
@@ -144,7 +136,8 @@ public class CommandParser {
 
     private String[] checkFlexi(String commandArguments) {
         for (int i = 0; i < FLEXI_KEYWORDS.length; i++) {
-            commandArguments = commandArguments.replace(FLEXI_KEYWORDS[i], ARGUMENT_SPLITTER);
+            commandArguments = commandArguments.replace(FLEXI_KEYWORDS[i],
+                    ARGUMENT_SPLITTER);
         }
         return commandArguments.split(ARGUMENT_SPLITTER);
     }
