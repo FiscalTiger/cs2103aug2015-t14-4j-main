3f257c7dcab1ee46d76fb08b0277dbc612f1eaa6
nicholasluimy <nicholasluimy@gmail.com>
2015-10-22 07:28:48 +0800

    tests for StorageManager

---
+++ src/easycheck/storage/StorageManagerTest.java
@@ -0,0 +1,67 @@
+package easycheck.storage;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import easycheck.logicController.LogicController;
+
+public class StorageManagerTest {
+	
+	
+	
+	@Test
+	public void testStorageManager() {
+		String testFileName1 = "testFile1.txt";
+		String testFileName2 = "testFile2.txt";
+		File testFile1 = new File(testFileName1);
+		try {
+			testFile1.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		StorageManager testManager1 = new StorageManager(testFileName1);
+		assertEquals(true, testFile1.exists());
+		
+		StorageManager testManager2 = new StorageManager(testFileName2);
+		File testFile2 = new File(testFileName2);
+		try {
+			testFile2.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		assertEquals(true, testFile2.exists());
+		
+		testFile1.delete();
+		testFile2.delete();
+	}
+
+	@Test
+	public void testReadDataFromEasyCheckFile() {
+		// note, will have to manually delete testFile.txt if test fail because it will not continue to the deletion
+		String testFileName = "testFile.txt";
+		StorageManager testManager = new StorageManager(testFileName);
+		assertEquals(true, testManager.readDataFromEasyCheckFile().isEmpty());
+		LogicController testLogic = new LogicController(testFileName);
+		testLogic.executeCommand("add firstTestTask");
+		assertEquals(1, testManager.readDataFromEasyCheckFile().size());
+		testLogic.executeCommand("add secondTestTask");
+		assertEquals(2, testManager.readDataFromEasyCheckFile().size());
+		testLogic.executeCommand("delete first");
+		assertEquals(1, testManager.readDataFromEasyCheckFile().size());
+		assertEquals(2, testManager.readDataFromEasyCheckFile().get(0).getEventIndex());
+		assertEquals("secondTestTask", testManager.readDataFromEasyCheckFile().get(0).getEventName());
+		File testFile = new File(testFileName);
+		testFile.delete();
+	}
+
+	@Test
+	public void testWriteDataToEasyCheckFile() {
+		// TODO this is currently tested by testReadData, using testLogic.executeCommand, of add type. change to manually add to file and read/write
+		
+	}
+
+}
