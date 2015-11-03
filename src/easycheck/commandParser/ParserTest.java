43f8402fa463a7a31f2af3683a26d9bc8bb718c5
Teo Qi Xuan <teoqixuan@hotmail.com>
2015-11-03 14:44:11 +0800

    updated parser test

--- src/easycheck/commandParser/ParserTest.java
+++ src/easycheck/commandParser/ParserTest.java
@@ -1,54 +1,79 @@
 package easycheck.commandParser;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
+import easycheck.commandParser.CommandTypes.Add;
+import easycheck.commandParser.CommandTypes.Invalid;
+
 /**
- * Simple JUnit Test for CommandParser,
+ * Simple JUnit Test for CommandParser
  * @@author A0124206W
  *
  */
 public class ParserTest {
-    
+    public static DateTimeFormatter formatter = DateTimeFormat
+            .forPattern("MM/dd/yyyy HH:mm");
+    public static DateTime testStartDate = formatter
+            .parseDateTime("11/12/2015 13:30");
+    /*
     @Test
     public void init() {
         CommandParser parser = new CommandParser();
     }
-    
+
     @Test
     public void addTask() {
         CommandParser parser = new CommandParser();
-        Command add = parser.parseCommand("add task name without due date");
-        assertEquals("task name without due date", add.getCommandArguments()[0]);
+        Add add = (Add) parser.parseCommand("add task name without due date");
+        assertEquals("task name without due date", add.getTaskName());
+        assertEquals(false, add.hasStart());
+        assertEquals(false, add.hasEnd());
     }
-    
+
     @Test
     public void addTaskWithDeadline() {
         CommandParser parser = new CommandParser();
-        Command add = parser.parseCommand("add task name, 11.12.2015 13:30");
-        assertEquals("task name", add.getCommandArguments()[0]);
-        assertEquals("11.12.2015 13:30", add.getCommandArguments()[1]);
+        Add add = (Add) parser.parseCommand("add task name, 11/12/2015 13:30");
+        assertEquals("task name", add.getTaskName());
+        assertEquals(testStartDate, add.getEnd());
+        assertEquals(false, add.hasStart());
     }
-    
+
     @Test
+    // Tests parsing of flexi-words such as by
     public void addTaskWithFlexiWords() {
         CommandParser parser = new CommandParser();
-        Command add = parser.parseCommand("add task name by 11.12.2015 13:30");
-        assertEquals("task name", add.getCommandArguments()[0]);
-        assertEquals("11.12.2015 13:30", add.getCommandArguments()[1]);
+        Add add = (Add) parser
+                .parseCommand("add task name by 11/12/2015 13:30");
+        assertEquals("task name", add.getTaskName());
+        assertEquals(testStartDate, add.getEnd());
+        assertEquals(false, add.hasStart());
     }
-    
-    //13th month is a boundary value for month
-    @Test (expected = org.joda.time.IllegalFieldValueException.class)
+*/
+    // 13th month is a boundary value for month
+    @Test
     public void addBadTaskThirteenthMonth() {
         CommandParser parser = new CommandParser();
-        Command add = parser.parseCommand("add task name by 11.13.2015 13:30");
+        Command invalid = parser
+                .parseCommand("add task name by 13/11/2015 13:30");
+        assertTrue(invalid instanceof Add);
+        assertTrue(invalid instanceof Invalid);
     }
-    
-    //0th month is a boundary value for month
-    @Test (expected = org.joda.time.IllegalFieldValueException.class)
+
+    // 0th month is a boundary value for month
+
+    @Test
     public void addBadTaskZeroMonth() {
         CommandParser parser = new CommandParser();
-        Command add = parser.parseCommand("add task name by 11.0.2015 13:30");
+        Command invalid = parser
+                .parseCommand("add task name by 0/11/2015 13:30");
+        assertTrue(invalid instanceof Invalid);
     }
+
 }
